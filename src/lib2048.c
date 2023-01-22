c0311b486baeabb400e924fea8cb55010fecb6c2
tip <zacariaz@gmail.com>
2023-01-22 13:31:04 +0100

    basic helper functions

--- src/lib2048.c
+++ src/lib2048.c
@@ -1,1 +1,37 @@
 #include "lib2048.h"
+#define TILE_SCORE( n ) ( (n)?((n)-1)<<(n):0 )
+#define TILE_MOVES( n ) ( (n)?1<<((n)-1):0 )
+
+Board new_game( void )
+{
+    Board board = 0ull;
+    return board;
+}
+Board move( Board board, Direction direction )
+{
+    return board;
+}
+int score( Board board )
+{
+    int score = 0;
+    do {
+        score += TILE_SCORE( board&0xf );
+    } while( board >>= 4 );
+    return score;
+}
+int moves( Board board )
+{
+    int moves = 0;
+    do {
+        moves += TILE_MOVES( board&0xf );
+    } while( board >>= 4 );
+    return moves-2;
+}
+int greatest( Board board )
+{
+    int greatest = 0;
+    do {
+        if( ( board&0xf ) > greatest ) greatest = ( board&0xf );
+    } while( board >>= 4 );
+    return greatest;
+}
--- src/lib2048.h
+++ src/lib2048.h
@@ -1,5 +1,14 @@
 #ifndef LIB2048_H
 #define LIB2048_H
 
+typedef unsigned long long Board;
+typedef enum { LEFT, UP, RIGHT, DOWN } Direction;
+
+Board new_game( void );
+Board move( Board, Direction );
+int score( Board );
+int moves( Board );
+int greatest( Board );
+
 
 #endif//LIB2048_h
